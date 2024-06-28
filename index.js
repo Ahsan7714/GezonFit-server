1fc6a36f077e690f33b5074b1ad64e73eff5fad9
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-06-28 18:45:04 +0500

    .

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://health-and-fitness-ashy.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
