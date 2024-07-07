ad7241ca399eb4bcb9dc7cbfe063726515909953
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-07-07 21:09:51 +0500

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,15 +74,18 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 
 // logout user
 exports.logoutUser = catchAsyncError(async (req, res, next) => {
-    res.cookie('token', null, {
-        expires: new Date(Date.now()),
-        httpOnly: true
-    });
-
-    res.status(200).json({
-        success: true,
-        message: 'Logged out successfully'
-    });
+    res.cookie("token", "", {
+        expires: new Date(0),
+        path: "/",
+        secure: true,
+        sameSite: "None",
+      });
+    
+      // Set Cache-Control header to prevent caching
+      res.setHeader("Cache-Control", "no-store");
+    
+      console.log("Cookie cleared.");
+      res.status(200).json({ success: true, message: "Logged out successfully" });
 });
 
 // post  a pending partner
